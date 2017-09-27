9026e80d12c3cba6a72abfda7cce37b7b98c5ce5
Robert <min.xia@quvideo.com>
2017-09-27 20:10:09 +0800

    first commint

---
+++ ios/Pods/Target Support Files/Pods-Snapvote/Pods-Snapvote-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Snapvote : NSObject
+@end
+@implementation PodsDummy_Pods_Snapvote
+@end
---
+++ ios/Pods/Target Support Files/Pods-SnapvoteTests/Pods-SnapvoteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapvoteTests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapvoteTests
+@end
